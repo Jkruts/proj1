e6e495d8dc9b56764dec9774c6b1e20fe6125d5b
Yevheniy <nitrutsa.e@gmail.com>
2017-11-26 22:04:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html>
+<title>Resume</title>
+<head>
+<meta charset="utf-8">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div class='float-right'>
+<img src="http://s.gama-gama.ru/userupload/138/04158cbb36c1ad60e26e3ab9d2f5ccff.jpg" alt="Фото кандидата" width='140px' height='140px'>
+	<div class='social-button'>
+		<a href='https://twitter.com/?lang=uk'>
+			<img src='http://redhint.com/wp-content/uploads/2017/09/Twitter.jpg' width='30px'height='30px'>
+		</a> <!--Посилання на Twitter-->
+		<a href='https://uk-ua.facebook.com/'>
+			<img src='http://www.amnet-systems.com/wp-content/uploads/2017/05/Facebook-Logo.png' width='30px'height='30px'>
+		</a> <!--Посилання на Facebook-->
+		<a href='https://www.google.com.ua/'>
+			<img src='https://www.seeklogo.net/wp-content/uploads/2015/09/google-plus-new-icon-logo.png' width='30px'height='auto'>
+		</a> <!--Посилання на Telegram-->
+    </div>
+</div>
+
+<div class='header'>
+    <h1>Євгеній Леонідович Нітруца</h1>
+    <ul>
+        <li>Рік народження: 1984, місце проживання: м. Львів, вул. Караїмська 12.</li>
+        <li>Телефон: 093-130-17-76.</li> 
+        <li>E-mail: <a href='nitrutsa.e@gmail.com'>nitrutsa.e@gmail.com</a></li>
+        
+    </ul>
+</div>
+
+<hr>
+
+<h2>Досвід роботи</h2>
+<p>Менеджер по продажам у салоні “Лео Кераміка”, м. Львів.
+З 03.2015 р. до сьогодні.<br>
+Продаж керамічної плитки та сантехніки.</p>
+
+<p>Менеджер по продажам у супермаркеті будівельних матеріалів “Орегон”, по сумісництву адміністратор корпоративного сайту на CMS Joomla. м. Ялта.<br>
+Продаж керамічної плитки, підтримка в робочому стані сайту компанії.</p>
+
+<h2>Освіта</h2>
+<p>Не закінчена вища з 09.2007 по 07.2011 р. Кримський Державний Університет, факультет менеджменту, м. Ялта.</p>
+<p>Середня спеціальна з 09.2004 по 02.2006 р. Херсонський комерційний технікум, факультет технології харчування, м. Херсон.</p>
+
+<h2>Додаткова освіта</h2>
+ <p>Онлайн-курс основи тестування програмного забезпечення 05.2016 р., QATestLab.</p>
+<p>Онлайн-курс на Prometheus - основи тестування програмного забезпечення 07.2016 р., Lviv IT Shcool. Сертифікат.</p>
+<p>Онлайн-курс по HTML/CSS - 2015 р. від HTML Academy.</p>
+
+<h2>Навички</h2>
+<p>Впевнене володіння ПК Windows, Ubuntu, можу створити сайт на CMS Wordpress з допомогою візуального редактора. Самостійно вивчаю мови програмування PHP, Java Script.</p>
+
+<h2>Володіння мовами</h2>
+<ul>
+    <li>Українська - добре;</li>
+    <li>Російська - добре;</li>
+    <li>Англійська - Elementary (in process).</li>
+</ul>
+
+
+<h2>Додаткова інформація</h2>
+<p>Цілеспрямований, посидючий, люблю навчатися, добре вживаюся з людьми, не конфліктний, чесний, вмію слухати.</p>
+
+</body>
+</html>
